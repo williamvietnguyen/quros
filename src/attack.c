1bf34f7626d14d6acde3b6322aacd90eabf6aeae
williamvietnguyen <williamvnguyen2@gmail.com>
2022-05-17 21:53:00 -0700

    Remove redundant loops

--- src/attack.c
+++ src/attack.c
@@ -24,17 +24,15 @@ static U64 white_pawn_west_attack(U64 b) {return (b & NOT_A_FILE) >> 9;}
 static U64 black_pawn_east_attack(U64 b) {return (b & NOT_H_FILE) << 9;}
 static U64 black_pawn_west_attack(U64 b) {return (b & NOT_A_FILE) << 7;}
 
+/* Reference: https://www.chessprogramming.org/Pawn_Attacks_(Bitboards) */
 static void init_pawn_attacks_masks() {
-    for (int rank = 0; rank < RANKS; rank++) {
-        for (int file = 0; file < FILES; file++) {
-            int square = get_square_number(rank, file);
-            U64 b = 0ULL;
-            set_bit(&b, square);
-            west_pawn_attacks[WHITE][square] = white_pawn_west_attack(b);
-            east_pawn_attacks[WHITE][square] = white_pawn_east_attack(b);
-            west_pawn_attacks[BLACK][square] = black_pawn_west_attack(b);
-            east_pawn_attacks[BLACK][square] = black_pawn_east_attack(b);
-        }
+    for (int square = 0; square < NUM_SQ; square++) {
+        U64 b = 0ULL;
+        set_bit(&b, square);
+        west_pawn_attacks[WHITE][square] = white_pawn_west_attack(b);
+        east_pawn_attacks[WHITE][square] = white_pawn_east_attack(b);
+        west_pawn_attacks[BLACK][square] = black_pawn_west_attack(b);
+        east_pawn_attacks[BLACK][square] = black_pawn_east_attack(b);
     }
 }
 
@@ -48,23 +46,21 @@ static U64 no_we_we(U64 b) {return (b & NOT_AB_FILE) <<  6;}
 static U64 so_we_we(U64 b) {return (b & NOT_AB_FILE) >> 10;}
 static U64 so_so_we(U64 b) {return (b & NOT_A_FILE ) >> 17;}
 
+/* Reference: https://www.chessprogramming.org/Knight_Pattern */
 static void init_knight_attacks_masks() {
-    for (int rank = 0; rank < RANKS; rank++) {
-        for (int file = 0; file < FILES; file++) {
-            int square = get_square_number(rank, file);
-            U64 b = 0ULL;
-            set_bit(&b, square);
-            U64 mask = 0ULL;
-            mask |= no_no_ea(b);
-            mask |= no_ea_ea(b);
-            mask |= so_ea_ea(b);
-            mask |= so_so_ea(b);
-            mask |= no_no_we(b);
-            mask |= no_we_we(b);
-            mask |= so_we_we(b);
-            mask |= so_so_we(b);
-            knight_attacks[square] = mask;
-        }
+    for (int square = 0; square < NUM_SQ; square++) {
+        U64 b = 0ULL;
+        set_bit(&b, square);
+        U64 mask = 0ULL;
+        mask |= no_no_ea(b);
+        mask |= no_ea_ea(b);
+        mask |= so_ea_ea(b);
+        mask |= so_so_ea(b);
+        mask |= no_no_we(b);
+        mask |= no_we_we(b);
+        mask |= so_we_we(b);
+        mask |= so_so_we(b);
+        knight_attacks[square] = mask;
     }
 }
 
